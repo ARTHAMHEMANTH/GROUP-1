513dd3b575b1e8dd0f4436d8b6082d04cc925ac4
ARTHAM HEMANTH <arthamhemanth001@gmail.com.com>
2025-02-05 10:26:49 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 // import Navbar from "./Components/Navbar";
 // import Main from "./Components/Main";
+// import Sidebar1 from "./components/Sidebar1";
+// import Sidebar2 from "./Components/Sidebar2";
+// import Footer from "./Components/Footer";
+
+// const App=()=>{
+//     return(
+//         <div classname="app">
+//             <Navbar/>
+//             <Main></Main>
+//             <div classname="sidebar">
+//                 <Sidebar1/>
+//                 <Sidebar2/>
+//             </div>
+//             <Footer></Footer>
+//         </div>
+//     )
+// }
+// export default App;
+// import Navbar from "./Components/Navbar";
+// import Main from "./Components/Main";
 // // import Sidebar1 from "./Components/Sidebar1";
 // import Sidebar2 from "./Components/Sidebar2";
 // import Footer from "./Components/Footer";
@@ -21,8 +41,8 @@
 // export default App;
 
 
-import React, { Component } from 'react'
-import CBSPropex1 from "./proposexample/CBSPropex1";
+// import React, { Component } from 'react'
+// import FBCPropex1 from "./proposexample/FBCPropex1";
 //   class APP extends Component {
 //     render() {
 //         return(
@@ -40,16 +60,50 @@ import CBSPropex1 from "./proposexample/CBSPropex1";
 //     }
 //   }
 //   export default APP;
-const APP=()=>{
+// const APP=()=>{
+//     return(
+//         <div>
+//             <FBCPropex1
+//             username="hemanth"
+//             isLoggedin={false}
+//             hobbies={["games","movies","books","study"]}
+//             />
+//         </div>
+//     )
+// }
+// export default APP;
+
+// import React from 'react'
+// import FBCPropex1 from './propsexample/FBCPropex1';
+
+
+// const App = () => {
+//     return (
+//         <div>
+//             <FBCPropex1
+//             username="HEMANTH"
+//             isLoggedIn={false}
+//             hobbies={["Roaming" , "Watching movies" , "travel" , "studies"]}
+//             />
+
+//         </div>
+//     )
+
+// }
+// export default App;
+
+import React from "react";
+import PropsChildrenex from './proposexample/PropsChildrenex';
+import ChildProps from './proposexample/ChildProps';
+
+const App = () => {
     return(
         <div>
-            <CBSPropex1
-            username="hemanth"
-            isLoggedin={false}
-            hobbies={["games","movies","books","study"]}
-            />
+            <PropsChildrenex username="Hemanth" company="Amazon">
+                <h1>This data is passed as props children</h1>
+                <ChildProps />
+            </PropsChildrenex>
         </div>
     )
 }
-export default APP;
-
+export default App
--- src/proposexample/CBCProposex1.jsx
+++ src/proposexample/CBCProposex1.jsx
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-  class CBCProposex1 extends Component {
-    render(){
-        console.log(this);
-        return(
-            <div>
-                CBCPropose1
-                <h1>{this.props.username}</h1>
-                <h2>{this.props.age}</h2>
-                <div>
-                    {this.props.design[0]}
-                </div>
-                <h2>{this.props.userDetails.area}</h2>
-                <button onClick={this.props.sendFun}>click on butoon</button>
+//   class CBCProposex1 extends Component {
+//     render(){
+//         console.log(this);
+//         return(
+//             <div>
+//                 CBCPropose1
+//                 <h1>{this.props.username}</h1>
+//                 <h2>{this.props.age}</h2>
+//                 <div>
+//                     {this.props.design[0]}
+//                 </div>
+//                 <h2>{this.props.userDetails.area}</h2>
+//                 <button onClick={this.props.sendFun}>click on butoon</button>
 
-            </div>
-        )
-    }
-  }
-  export default CBCProposex1;
+//             </div>
+//         )
+//     }
+//   }
+//   export default CBCProposex1;
