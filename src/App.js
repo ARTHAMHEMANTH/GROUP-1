d1dfccc3faebbe9d2c5de9145b3562ef493fcee8
ARTHAM HEMANTH <arthamhemanth001@gmail.com.com>
2025-02-12 23:59:43 +0530

    react

--- src/App.js
+++ src/App.js
@@ -171,17 +171,55 @@
 
 // export default App
 
-import React from 'react'
+// import React from 'react'
 // import Exuseeffect from './hooks/Exuseeffect'
-import Refexamples from './hooks/Refexamples'
+// import Refexamples from './hooks/Refexamples'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <hr />
+//       <Exuseeffect></Exuseeffect>
+//       <Refexamples></Refexamples>
+//     </div>
+//   )
+// }
+// export default App
+import React from 'react'
+import Navbar from './Components/Navbar'
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import ClassRoom from './pages/ClassRoom';
+import Assessment from './pages/Assessment';
+import Store from './pages/Store';
+import Calender from './pages/Calender';
+import News from './pages/News';
+import Blog from './pages/Blog';
+import PageNotFound from './pages/PageNotFound';
+import Profile from './pages/Profile';
+import MyAccounts from './pages/MyAccounts';
+import MySettings from './pages/MySettings';
 
 const App = () => {
   return (
-    <div>
-      <hr />
-      {/* <Exuseeffect></Exuseeffect> */}
-      <Refexamples></Refexamples>
-    </div>
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/classroom' element={<ClassRoom/>}/>
+            <Route path='/assessment' element={<Assessment/>}/>
+            <Route path='/store' element={<Store/>}/>
+            <Route path='/calender' element={<Calender/>}/>
+            <Route path='/news' element={<News/>}/>
+            <Route path='/blog' element={<Blog/>}/>
+            <Route path='/profile' element={<Profile/>}>
+                <Route path='myaccount' element={<MyAccounts/>}/>
+                <Route path='mysettings' element={<MySettings/>}/>
+            </Route>
+            <Route path='*' element={<PageNotFound/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
+
 export default App
